fd86779156142bf4268b6a04dd6fc0db2a3b48bc
lfzizr <2027325028@qq.com>
2025-07-21 19:39:22 +0800

    first

---
+++ apps/user/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "gitee.com/lfzizr/easy-chat/apps/user/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
